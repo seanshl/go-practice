4650aa8642442a8b775e79a56a5ba2de8aa25468
Sean <liushiyao2014@gmail.com>
2021-11-17 22:38:40 -0800

    some summary

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,8 @@
 package main
 
+// files with same package can be treated as single file in the end
+// aka, you can call funcs from another file under the same package
+
 import "fmt"
 
 func main() {
@@ -8,8 +11,15 @@ func main() {
 
 	fmt.Println(cards)
 
+	// range is a keyword not a func
+
 	for i, card := range cards {
 		fmt.Println(i, card)
+	}
+
+	for _, card := range cards {
+		fmt.Println(card)
+		// every declared variable must be used
 	}
 
 }
